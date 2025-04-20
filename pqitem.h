156ebc4dca4f43b4e2861a9fe5ea6300432190b9
mo-alowais <m.alowais24@gmail.com>
2025-04-20 15:27:22 +0400

    Added aging priority by adding an enqueue time to the struct, and increment at each insert

--- pqitem.h
+++ pqitem.h
@@ -3,6 +3,7 @@
 // This item struct is defined to enable assignment of custom priorities
 typedef struct{
     int priority;
+    long enqueue_time;
     int* data;
 }PQItem;
 
--- priorityQueue.c
+++ priorityQueue.c
@@ -3,7 +3,8 @@
 
 #include "priorityQueue.h"
 #include "utils.h"
-
+#define AGING_FACTOR 1
+static long logical_time = 0; // updates each insert
 // Swap two elements
 void swap(PQItem *a, PQItem *b){
     PQItem temp = *a;
@@ -25,6 +26,7 @@ int insert(PriorityQueue *pq, int *data, int priority){
     int i = pq->size++;
     pq->heap[i].priority = priority;
     pq->heap[i].data = data;
+    pq->heap[i].enqueue_time = logical_time++;
 
     while(i > 0 && pq->heap[i].priority > pq->heap[(i - 1) /2].priority){
         swap(&pq->heap[i], &pq->heap[(i-1)/2]);
@@ -35,6 +37,9 @@ int insert(PriorityQueue *pq, int *data, int priority){
 
 }
 
+int effective_priority(PQItem *item){
+    return item->priority + AGING_FACTOR * item->enqueue_time;
+}
 int *extract_max(PriorityQueue *pq){
     assert_value(pq->size>0, "Trying to extract from an empty heap");
 
@@ -43,15 +48,16 @@ int *extract_max(PriorityQueue *pq){
 
     int i = 0;
 
+
     // heapify after extracting
     while(1){
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
         int largest = i;
-        if (left < pq->size && pq->heap[left].priority > pq->heap[largest].priority)
+        if (left < pq->size && effective_priority(&pq->heap[left]) > effective_priority(&pq->heap[largest]))
             largest = left;
-        if (right < pq->size && pq->heap[right].priority > pq->heap[largest].priority)
+        if (right < pq->size && effective_priority(&pq->heap[right]) > effective_priority(&pq->heap[largest]))
             largest = right;
         if (largest == i)
             break;
@@ -61,7 +67,9 @@ int *extract_max(PriorityQueue *pq){
     }
     return out;
 }
-
+int effective_priority(PQItem *item) {
+    return item->priority + AGING_FACTOR * (logical_time - item->enqueue_time);
+}
 
 void destroy(PriorityQueue *pq){
     free(pq->heap);
